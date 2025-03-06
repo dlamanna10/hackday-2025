6c15721173b0bae880a4c5de20509af64fda241b
Dominic Lamanna <dlamanna10@gmail.com>
2025-03-06 16:45:03 -0600

    Apply button functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,17 @@ export default function Home() {
           </div>
           {/* Desktop Navigation */}
           <div className="hidden md:flex space-x-24 justify-center w-full">
-            {["Home", "Apply", "The Founder's Arena", "Schedule", "About Us", "FAQ", "Contact"].map((item) => (
-              <a key={item} href="#" className="font-franklinGothic text-xl font-normal hover:text-purple-300 transition">
-                {item}
+            {[
+              { name: "Home", href: "#" },
+              { name: "Apply", href: "#apply" },
+              { name: "The Founder's Arena", href: "#founders-arena" },
+              { name: "Schedule", href: "#schedule" },
+              { name: "About Us", href: "#about" },
+              { name: "FAQ", href: "#faq" },
+              { name: "Contact", href: "#contact" },
+            ].map((item) => (
+              <a key={item.name} href={item.href} className="font-franklinGothic text-xl font-normal hover:text-purple-300 transition">
+                {item.name}
               </a>
             ))}
           </div>
@@ -69,14 +77,14 @@ export default function Home() {
           <Image
             src="/BuildingGlow.svg"
             alt="Building Glow"
-            className="absolute bottom-[-480px] w-full h-auto z-10 building-glow-mask"
+            className="absolute bottom-[-480px] sm:bottom-[-300px] md:bottom-[-480px] w-full h-auto z-10 building-glow-mask"
             width={1438}
             height={730}
           />
           <Image
             src="/Buildings.svg"
             alt="Buildings"
-            className="absolute bottom-[-480px] w-full h-auto z-20 building-mask"
+            className="absolute bottom-[-480px] sm:bottom-[-300px] md:bottom-[-480px] w-full h-auto z-20 building-mask"
             width={1440}
             height={761}
           />
@@ -85,11 +93,19 @@ export default function Home() {
 
       {/* Information Section */}
       <div className="font-franklinCondensed text-white text-center w-[100vw] mx-auto p-[40px] mt-[-150px]">
-        <section className="mx-auto p-[50px]">
+        <section id='apply' className="scroll-mt-16 pt-10 mx-auto p-[50px]">
           <h2 className="text-5xl font-bold">ARE YOU READY TO GO BEYOND?</h2>
-          <p className="text-2xl mt-4">APPLY</p>
         </section>
-        <section className="mx-auto p-[50px]">
+        <section id='apply' className="mt-[-80px] mx-auto p-[70px]">
+        <a href='https://docs.google.com/forms/d/e/1FAIpQLScBIrfcTsXM32cx4RX1HQItdzIkzVIVj57EL1k5caWSw_btdA/viewform?usp=sharing'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='relative px-8 py-3 text-2xl font-semibold text-white rounded-full transition duration-300
+          bg-gradient-to-r from-purple-600 to-red-500 hover:from-purple-600 hover:to-blue-600 shadow-lg hover:shadow-xl' >
+            APPLY NOW
+          </a>
+        </section>
+        <section id='founders-arena' className="scroll-mt-16 pt-10 mx-auto p-[50px]">
           <h2 className="text-5xl font-bold">THE FOUNDER'S ARENA</h2>
           <p className="text-2xl mt-4 leading-relaxed">
             The Founders Arena is a unique and first-of-its-kind "go-to-market"
@@ -99,7 +115,11 @@ export default function Home() {
             quicker growth...
           </p>
         </section>
-        <section className="mx-auto p-[50px]">
+        <section id='schedule' className="scroll-mt-16 pt-10 mx-auto p-[50px]">
+          <h2 className="text-5xl font-bold">HackDay Schedule</h2>
+          <p className="text-2xl mt-4">W.I.P.</p>
+        </section>
+        <section id='about' className="scroll-mt-16 pt-10 mx-auto p-[50px]">
           <h2 className="text-5xl font-bold">Meet the Team</h2>
           <p className="text-2xl mt-4">HackUTA Team Information</p>
         </section>
