62be292ed2c5e20bebeec2b2176ecd34148011e9
dlamanna10 <dlamanna10@gmail.com>
2025-03-07 09:04:34 -0600

    White text on navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
               { name: "FAQ", href: "#faq" },
               { name: "Contact", href: "#contact" },
             ].map((item) => (
-              <a key={item.name} href={item.href} className="font-franklinGothic text-xl font-normal hover:text-purple-300 transition">
+              <a key={item.name} href={item.href} className="font-franklinGothic text-white text-xl font-normal hover:text-purple-300 transition">
                 {item.name}
               </a>
             ))}
